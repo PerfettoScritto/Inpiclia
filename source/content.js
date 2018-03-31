57bc1d94839772e3f9c76a7e866d5bddc5e343dd
Alexander Mityaev <PerfettoScritto@users.noreply.github.com>
2018-03-31 10:04:48 +0200

    Add functionality to the extension

--- source/content.js
+++ source/content.js
@@ -1,22 +1,140 @@
 $(document).ready(function() {
 
-var imageBlocks = document.getElementsByClassName("_mck9w _gvoze _tn0ps");
 
-var imageBlockUrl = imageBlocks[1].firstChild.getAttribute("href");
+    var imageBlocks = document.getElementsByClassName("_mck9w _gvoze _tn0ps");
 
-imageBlockUrl = "https://www.instagram.com" + imageBlockUrl;
 
-var position = imageBlockUrl.indexOf("/?");
+    // Set links for images.
+    // This block is needed for the first downloading of web page.
 
-var bigImageUrl = imageBlockUrl.substring(0, position +1) + "media?size=l";
 
-console.log(bigImageUrl);
+    for (i = 0; i < imageBlocks.length; i++) {
 
-// document.location.replace(url);
 
-console.log(imageBlockUrl);
-//+ "media?size=l"
-console.log('hello');
+        var imageBlockUrl = imageBlocks[i].firstChild.getAttribute("href");
+
+        imageBlockUrl = "https://www.instagram.com" + imageBlockUrl;
+
+        var position = imageBlockUrl.indexOf("/?");
+
+        var bigImageUrl = imageBlockUrl.substring(0, position + 1) + "media?size=l";
+
+        createImgLink(imageBlocks[i], bigImageUrl);
+
+        //additional functionality for later update
+
+        // createOpenBlockLink (imageBlocks[i], imageBlockUrl);
+
+    }
+
+
+    setInterval(updateLinks, 1000);
+
+
+    //Update links for images downloading
+
+    function updateLinks() {
+
+
+        var imageBlocks = [];
+
+        var links = [];
+
+
+        imageBlocks = document.getElementsByClassName("_mck9w _gvoze _tn0ps");
+
+        for (i = 0; i < imageBlocks.length; i++) {
+
+            var imageBlockUrl = imageBlocks[i].firstChild.getAttribute("href");
+
+            imageBlockUrl = "https://www.instagram.com" + imageBlockUrl;
+
+            var position = imageBlockUrl.indexOf("/?");
+
+            var bigImageUrl = imageBlockUrl.substring(0, position + 1) + "media?size=l";
+
+            var links = imageBlocks[i].getElementsByTagName("a");
+
+            for (y = 0; y < links.length; y++) {
+
+                var link = links[y];
+
+                var classLink = link.className;
+
+                if (classLink === "linkview" || classLink === "linkblock") {
+
+                    // console.log(imageBlocks[i]);
+
+                    imageBlocks[i].removeChild(link);
+
+                }
+
+                //console.log(classLink);
+
+            }
+
+            createImgLink(imageBlocks[i], bigImageUrl);
+
+            // additional functionality for later update
+            // createOpenBlockLink (imageBlocks[i], imageBlockUrl );
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+    //Function adds download link for image
+
+    function createImgLink(needBlock, imageLink) {
+
+        var nodeA = document.createElement("a");
+
+        var textNodeA = document.createTextNode("download");
+
+        nodeA.setAttribute('href', imageLink);
+
+        nodeA.setAttribute('class', "linkview");
+
+        nodeA.setAttribute('target', "_blank");
+
+        nodeA.appendChild(textNodeA);
+
+        needBlock.appendChild(nodeA);
+
+    }
+
+
+    // additional functionality for later update
+    // Open image block in new tab
+
+
+    function createOpenBlockLink(needBlock, blockLink) {
+
+        var nodeA = document.createElement("a");
+
+        var textNodeA = document.createTextNode("open in tab");
+
+        nodeA.setAttribute('href', blockLink);
+
+        nodeA.setAttribute('class', "linkblock");
+
+        nodeA.appendChild(textNodeA);
+
+        needBlock.appendChild(nodeA);
+
+    }
+
+
+    // console.log(bigImageUrl);
+
+    // console.log(imageBlockUrl);
+    //+ "media?size=l"
+
 });
-
-
